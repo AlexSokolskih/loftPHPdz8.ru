645ba29beb8920b044bf99fe80c06fa1ec04bc9d
AlexSokolskih <xaam1@ya.ru>
2017-07-13 20:56:36 +0300

    Initial commit

---
+++ app/core/router.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 13.07.2017
+ * Time: 20:51
+ */
+class Router
+{
+
+}
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Alex
+ * Date: 13.07.2017
+ * Time: 14:54
+ */
+
+require_once 'vendor/autoload.php';
